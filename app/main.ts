7fda66a63c185c03a61abc4305bc63454b66af1b
chiefkarim <krimotop2016@gmail.com>
2024-09-22 18:11:02 +0100

    feat: uncomment code

--- app/main.ts
+++ app/main.ts
@@ -16,12 +16,9 @@ if (args[2] !== "-E") {
   process.exit(1);
 }
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
-// if (matchPattern(inputLine, pattern)) {
-//   process.exit(0);
-// } else {
-//   process.exit(1);
-// }
+if (matchPattern(inputLine, pattern)) {
+  process.exit(0);
+} else {
+  process.exit(1);
+}
